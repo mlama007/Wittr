cc467e047490694dab30e9aeaeeed94abd04b7f4
Maria Lamardo <mlamardo@Marias-MacBook-Pro.local>
2018-07-14 13:56:37 -0400

    Safe first 30 posts

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -168,8 +168,15 @@ IndexController.prototype._onSocketMessage = function(data) {
     //
     // Hint: you can use .openCursor(null, 'prev') to
     // open a cursor that goes through an index/store
-    // backwards.
+    // backwards.    
+    store.index('by-date').openCursor(null, 'prev')
+    .then(function(cursor){
+      return cursor.advance(30);
+    }).then(function deleteRest(cursor){
+      if (!cursor) return;
+      cursor.delete();
+      return cursor.continue();
+    });
   });
-
   this._postsView.addPosts(messages);
 };
